183cb432323804bf8a46618280be5320ff3dc652
Jaymin Parmar <jayminparmar2884@gmail.com>
2024-02-16 19:14:49 -0500

    first commit

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,4 +1,5 @@
 import { signOut } from 'next-auth/react';
+import Link from 'next/link'; // Import Link from next/link
 import React from 'react';
 
 import useCurrentUser from '@/hooks/useCurrentUser';
@@ -16,13 +17,15 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
 
   return (
     <div className="bg-white rounded-md border border-gray-200 shadow-lg w-96 absolute top-16 right-4 p-2 flex flex-col">
-      <div className="flex items-center mb-2">
-        <img className="w-16 h-16 rounded-full" src="/images/avtar.png" alt="User Avatar" />
-        <div className="ml-6">
-          <p className="text-xl font-semibold text-gray-800">{currentUser?.name}</p>
-          <p className="text-sm text-gray-600">{currentUser?.email}</p>
+      <Link href="/userProfile">
+        <div className="flex items-center mb-2 cursor-pointer">
+          <img className="w-16 h-16 rounded-full" src="/images/avtar.png" alt="User Avatar" />
+          <div className="ml-6">
+            <p className="text-xl font-semibold text-gray-800">{currentUser?.name}</p>
+            <p className="text-sm text-gray-600">{currentUser?.email}</p>
+          </div>
         </div>
-      </div>
+      </Link>
       <hr className="border-t border-gray-200 mb-2" />
       
       <div
